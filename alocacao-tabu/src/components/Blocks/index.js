6b62662160787a2ab80badab0a244c736b0d1406
Luan Barbosa <luanxpg9@gmail.com>
2021-06-07 23:59:59 -0300

    importando imagem nos components

---
+++ alocacao-tabu/src/components/Blocks/index.js
@@ -0,0 +1,12 @@
+import "./index.css";
+import logo from "../../images/ufs_logo.jpg";
+
+function Blocks() {
+  return (
+    <div className="main-block">
+      <img src={logo} alt="Ufs logo" />
+    </div>
+  );
+}
+
+export default Blocks;
